9275695cacc1d76a66fe6bfd896c8c6174a42da9
Federall <bender92@list.ru>
2020-04-10 16:19:01 +0300

    НЕ понимаю что вернуть

--- Connector.java
+++ Connector.java
@@ -7,7 +7,7 @@ public class Connector {
     private final String URL = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false ";
 
 
-    public URL getConneсtion() throws SQLException {
+    public connection getConneсtion() throws SQLException {
         String USERNAME = "root";
         String PASSWORD = "18111992";
 
@@ -15,7 +15,6 @@ public class Connector {
             System.out.println("Устанавливаем соединение с базой");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
-            PreparedStatement preparedStatement = connection.prepareStatement();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -23,7 +22,7 @@ public class Connector {
             connection.close();
             System.out.println("Закрыли соединение с базой");
         }
-    }return URL;
+    }return ;
 
 }
 
