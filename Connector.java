9d094d4e247a94170042b4afb7bc66d0df691280
Federall <bender92@list.ru>
2020-04-10 19:58:26 +0300

    Испарвил

--- Connector.java
+++ Connector.java
@@ -1,20 +1,19 @@
 import java.sql.*;
 
 public class Connector {
-    private Connection connection = null;
     private final String URL = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false ";
     private final String USERNAME = "root";
     private final String PASSWORD = "18111992";
 
     public Connection getConneсtion() throws SQLException {
         try {
+            Connection connection = null;
             System.out.println("Устанавливаем соединение с базой");
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
         } catch (Exception e) {
             System.out.println("Соединение не удалось получить");
-            e.printStackTrace();
         }
-        return connection;
+        return null;
     }
 }
